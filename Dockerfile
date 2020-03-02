97c5fb1c57ba037f01dc5bf49ac58084f01c5f78
Josh Balfour <josh@joshbalfour.co.uk>
2020-03-02 19:52:49 +0000

    it fucking works omgggg

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM node
 RUN npm install -g @prisma/client
-RUN npm install -g prisma2
-
+RUN npm install -g prisma2@2.0.0-preview022
+COPY hello-prisma /app
+RUN cd /app && yarn
--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -6,40 +6,91 @@ const { Pool } = require('pg')
 const { createApolloFetch } = require('apollo-fetch')
 const jwt = require('jsonwebtoken')
 
+// const { Client } = require('prisma-cli-engine')
+const { PrismaDefinitionClass, Environment } = require('prisma-yml')
+
 const exec = util.promisify(require('child_process').exec)
 
-const mgmtFetch = async opts => {
+const mgmtFetch = createApolloFetch({
+    uri: process.env.PRISMA_MANAGEMENT_URL,
+})
+
+mgmtFetch.use(async ({ request, options }, next) => {
+    if (!options.headers) {
+      options.headers = {}  // Create the headers object if needed.
+    }
+
     const mgmtToken = await jwt.sign({ grants: [{ target: '*/*', action: '*' }] }, process.env.PRISMA_MANAGEMENT_SECRET, { expiresIn: '1h' })
-    const fetch = createApolloFetch({
-        uri: process.env.PRISMA_MANAGEMENT_URL,
-        headers: {
-            Authorization: `Bearer ${mgmtToken}`,
-        }
-    })
+    options.headers['authorization'] = `Bearer ${mgmtToken}`
 
-    return fetch(opts)
-}
+    next()
+})
 
-const createPrismService = (id) => {
+const createPrismService = async (id) => {
+    const cwd = `/tmp/${id}`
+    const definitionPath = `/tmp/${id}/prisma.yml`
+
+    const datamodelStr = await createBaseSchema(id)
+
+    const ymlStr = [
+        'datamodel: datamodel.graphql', 
+        `endpoint: ${process.env.PRISMA_MANAGEMENT_URL.split('/management').join('')}`,
+    ].join('\n')
+
+    await fse.outputFile(definitionPath, ymlStr)
+    await fse.outputFile(`${cwd}/datamodel.graphql`, datamodelStr)
+
     const input = {
         name: id,
         stage: 'live',
         secrets: [id],
     }
- 
-    return mgmtFetch({
-        mutation:`
-            mutation addProject($input: AddProjectInput!) {
+
+    const environment = new Environment(cwd)
+    const definition = new PrismaDefinitionClass(environment, definitionPath)
+    await definition.load({})
+
+    const { typesString } = definition
+
+    if (!typesString) {
+        throw new Error('typesString is falsey')
+    }
+
+    await mgmtFetch({
+        query:`mutation addProject($input: AddProjectInput!) {
                 addProject(input: $input) {
                     project {
                         name
                         stage
                     }
                 }
-            }
-        `,
+            }`,
         variables: { input },
     })
+
+    const deployInput = {
+        name: id,
+        stage: 'live',
+        types: typesString,
+    }
+
+    const res = await mgmtFetch({
+        query:`mutation deploy($name: String!, $stage: String!, $types: String!) {
+                deploy(input: { name: $name, stage: $stage, types: $types }) {
+                    errors {
+                        type
+                        field
+                        description
+                    }
+                    migration {
+                        status
+                    }
+                }
+            }`,
+        variables: deployInput,
+    })
+
+    return res
 }
 
 
@@ -56,35 +107,44 @@ const createPGUser = async id => {
     await pool.end()
 }
 
-const createBaseSchema = id => {
-    const template = fs.readFileSync('schema.prisma', 'utf-8')
-    const schema = template.split('$url').join(`postgresql://${id}:${id}@${process.env.PGHOST}:${process.env.PGPORT}/${id}`)
+const createBaseSchema = async id => {
+    // const template = await fse.readFile('schema.prisma', 'utf-8')
+    // const schema = template.split('$url').join(`postgresql://${id}:${id}@${process.env.PGHOST}:${process.env.PGPORT}/${id}`)
 
-    return schema
+    // return schema
+
+    return fse.readFile('datamodel.graphql', 'utf-8')
 }
 
-const runBaseMigration = async (id, schema) => {
-    const cwd = `/tmp/${id}`
-    await fse.outputFile(`${cwd}/schema.prisma`, schema)
-    // const zero = await exec(`npx prisma2 init`)
-    const one = await exec(`prisma2 migrate save --name "init ${id}" --experimental`, { cwd })
-    const two = await exec(`prisma2 migrate up --experimental`, { cwd })
+// const runBaseMigration = async (id, schema) => {
+//     const cwd = `/tmp/${id}`
+//     await fse.outputFile(`${cwd}/schema.prisma`, schema)
+//     const one = await exec(`prisma2 migrate save --name "init ${id}" --experimental`, { cwd, env: {
+//         ...process.env,
+//         DEBUG: '*',
+//     } })
+//     const two = await exec(`prisma2 migrate up --experimental`, { cwd })
 
-    return [one, two]
-}
+//     return [one, two]
+// }
 
 const addProject = async ({ id }) => {
     // create postgres user
-    await createPGUser(id)
-    // create prisma service
-    await createPrismService(id)
-    // create base schema.prisma
-    const schema = await createBaseSchema(id)
-    
-    // run migrate
-    const migrationResults = await runBaseMigration(id, schema)
+    // await createPGUser(id)
 
-    console.log(JSON.stringify(migrationResults, null, '\t'))
+    // create prisma service
+    const res = await createPrismService(id)
+    console.log(JSON.stringify(res, null, '\t'))
+
+    console.log('(probably) created', id)
+
+    // create base schema.prisma
+    // const schema = await createBaseSchema(id)
+    
+    // // run migrate
+    // const migrationResults = await runBaseMigration(id, schema)
+
+    // console.log(JSON.stringify(migrationResults, null, '\t'))
 
     return true
 }
