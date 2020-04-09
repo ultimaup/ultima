8705e883cde64a1dbea7c9a0e9375080c8cddc56
Sanguinaris <wieichdashasse@gmail.com>
2020-04-09 17:35:44 +0100

    did base s3 upload script thing

--- build.sh
+++ build.sh
@@ -23,6 +23,20 @@ cd ..
 tar -cvpJf ../build-${DRONE_COMMIT_SHA}.tar.xz .
 
 # upload backup to somewhere
-ip a
-ls -alh .
-ls -alh ../build-${DRONE_COMMIT_SHA}.tar.xz
+host=drone.ultima.re
+s3_key='secret key'
+s3_secret='secret token'
+
+resource="/build-artifacts/build-${DRONE_COMMIT_SHA}.tar.xz"
+content_type="application/octet-stream"
+date=`date -R`
+_signature="PUT\n\n${content_type}\n${date}\n${resource}"
+signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
+
+curl -v -X PUT -T "build-${DRONE_COMMIT_SHA}.tar.xz" \
+          -H "Host: $host" \
+          -H "Date: ${date}" \
+          -H "Content-Type: ${content_type}" \
+          -H "Authorization: AWS ${s3_key}:${signature}" \
+          http://${host}:9000${resource}
+
