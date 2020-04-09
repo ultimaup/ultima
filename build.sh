f099d3e57b77c4003dcb851adca22d7feeff9c7d
Sanguinaris <wieichdashasse@gmail.com>
2020-04-09 17:37:33 +0100

    fixed build name

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ date=`date -R`
 _signature="PUT\n\n${content_type}\n${date}\n${resource}"
 signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
 
-curl -v -X PUT -T "build-${DRONE_COMMIT_SHA}.tar.xz" \
+curl -v -X PUT -T "../build-${DRONE_COMMIT_SHA}.tar.xz" \
           -H "Host: $host" \
           -H "Date: ${date}" \
           -H "Content-Type: ${content_type}" \
