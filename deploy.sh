1b5322b4f4f882cd45fd88408b2f8c0f26bace40
Sanguinaris <wieichdashasse@gmail.com>
2020-04-10 15:32:29 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mode=${DRONE_BRANCH}
+
+echo $mode
+
+if [ $mode = "master" ]
+then
+        curl -X "POST"  http://ultima.re:4480/build-${DRONE_COMMIT_SHA}.tar.xz
+elif [ $mode = "staging" ]
+then
+        echo "is staging"
+fi
