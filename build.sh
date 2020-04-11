09cda0c30108ce1fe0da67a597265f4d4e2eecb1
Sanguinaris <wieichdashasse@gmail.com>
2020-04-11 21:28:16 +0100

    was firewall

--- build.sh
+++ build.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-ip a
+# get dependencies
+echo "Getting Endpoint deps"
+cd endpoints
+yarn install --frozen-lockfile
+cd ..
+
+echo "Getting file-manager deps"
+cd file-manager
+yarn install --frozen-lockfile
+cd ..
+
+echo "Getting mgmt deps"
+cd mgmt
+yarn install --frozen-lockfile
+
+echo "Getting builder/nodejs deps"
+cd builders/nodejs
+npm ci
+cd ../..
+
+cd ..
+
+echo "Getting deployer deps"
+cd deployer
+yarn install --frozen-lockfile
+cd ..
+
 echo "Creating tarball"
 # Create backup
 tar -cpJf ../build-${DRONE_COMMIT_SHA}.tar.xz --exclude .git .
@@ -23,7 +49,5 @@ curl -4 -v -X PUT -T "../build-${DRONE_COMMIT_SHA}.tar.xz" \
           -H "Content-Type: ${content_type}" \
           -H "Authorization: AWS ${s3_key}:${signature}" \
           http://${host}:9000${resource}
-
-
 
 echo "Done"
