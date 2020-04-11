b67c565f1ffd1e8b4a42d52ba8fd371b29499a43
Sanguinaris <wieichdashasse@gmail.com>
2020-04-11 21:41:44 +0100

    only push on important branches

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 mode=${DRONE_BRANCH}
 
 echo "Going to deploy ${mode}"
-curl -4 -X "POST"  http://ultima.re:4480/build-${DRONE_COMMIT_SHA}.tar.xz
 if [ $mode = "master" ]
 then
         curl -4 -X "POST"  http://ultima.re:4480/build-${DRONE_COMMIT_SHA}.tar.xz
