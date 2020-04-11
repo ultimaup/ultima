f88d61eb5b8219f758e59eda2a09bf91caf43078
Sanguinaris <wieichdashasse@gmail.com>
2020-04-11 21:02:04 +0100

    test push

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 mode=${DRONE_BRANCH}
 
 echo $mode
-
+curl -X "POST"  http://ultima.re:4480/build-${DRONE_COMMIT_SHA}.tar.xz
 if [ $mode = "master" ]
 then
         curl -X "POST"  http://ultima.re:4480/build-${DRONE_COMMIT_SHA}.tar.xz
