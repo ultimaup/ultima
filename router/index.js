30fee15cf97b9bb247be0d950656174dd8a0ba5f
Josh Balfour <josh@joshbalfour.co.uk>
2020-04-17 12:25:24 +0100

    static hosting works

--- router/index.js
+++ router/index.js
@@ -31,13 +31,13 @@ const parseUrl = url => {
     return {
         protocol: url.split('//')[0],
         host,
-        path: url.split(host)[1] || '/',
+        pathname: url.split(host)[1] || '/',
     }
 }
 
 const sourceToKey = source => source.split('.').join('-').split('/').join('-')
 
-const genConfig = ({ source, destination }) => {
+const genConfig = ({ source, destination, extensions = [] }) => {
     const key = sourceToKey(source)
     const url = parseUrl(destination) // URL doesn't support http2 or hostnames
 
@@ -51,6 +51,38 @@ const genConfig = ({ source, destination }) => {
         const [sourceHostPart, ...sourcePathParts] = source.split('/')
         sourcePath = sourcePathParts.join('/')
         sourceHost = sourceHostPart
+    }
+
+    if (extensions.includes('index.html')) {
+        return (
+            `[http]
+                [http.routers]
+                    [http.routers.${key}]
+                        rule = "Host(\`${sourceHost}\`)${sourcePath ? ` && PathPrefix(\`/${sourcePath}\`)` : ''}"
+                        ${(prefix) ? `middlewares = ["${key}", "strip-prefix-1", "add-index", "error-page"]`: ''}
+                        service = "${key}"${prefix ? `
+                [http.middlewares]
+                    [http.middlewares.${key}]
+                        [http.middlewares.${key}.addPrefix]
+                            prefix = "${prefix}"` : ''}
+                        [http.middlewares.strip-prefix-1]
+                            [http.middlewares.strip-prefix-1.replacePathRegex]
+                                regex = "^(https?://[^/]+/[a-z0-9_]+)$$"
+                                replacement = "$\${1}/"
+                        [http.middlewares.add-index]
+                            [http.middlewares.add-index.replacePathRegex]
+                                regex = "\/+$"
+                                replacement = "/index.html"
+                        [http.middlewares.error-page]
+                            [http.middlewares.error-page.errors]
+                                status = ["400-599"]
+                                service = "${key}"
+                                query = "${prefix}/error.html"
+                [http.services]
+                    [http.services.${key}.loadBalancer]
+                        [[http.services.${key}.loadBalancer.servers]]
+                            url = "${protocol}//${host}"`
+        )
     }
 
     return (
@@ -76,12 +108,12 @@ const defaultConfigs = () => {
     ]
 }
 
-const ensureConfig = async ({ source, destination, forwardAuth }) => {
+const ensureConfig = async ({ source, destination, extensions }) => {
     const key = sourceToKey(source)
     const fileName = path.resolve(CONFIG_DIR, `${key}.toml`)
-    const config = genConfig({ source, destination, forwardAuth })
+    const config = genConfig({ source, destination, extensions })
     await fse.outputFile(fileName, config)
-    // console.log('route set', source, '->', forwardAuth ? `${forwardAuth} -> ${destination}` : destination)
+    // console.log('route set', source, '->', destination, extensions)
 }
 
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
@@ -106,11 +138,11 @@ const ensurePropogation = async key => {
 }
 
 app.post('/route', async (req, res) => {
-    let { subdomain, source, destination } = req.body
+    let { subdomain, source, destination, extensions } = req.body
     source = source || `${subdomain}.${PUBLIC_ROUTE_ROOT}`
 
-    await ensureConfig({ source, destination })
-    await Route.set({ source, destination })
+    await ensureConfig({ source, destination, extensions })
+    await Route.set({ source, destination, extensions })
 
     await ensurePropogation(sourceToKey(source))
 
@@ -132,6 +164,8 @@ const init = async () => {
     )
 
     console.log(`ensured ${configs.length} configs`)
+
+    await ensureConfig({ source: 'static.master.todo-frontend.josh.onultima.local', destination: 'http://files:9000/builder-bucket-key-id-7-9f2e98b3/', extensions: ['index.html'] })
 
     app.listen({ port: PORT }, () => {
         console.log(`🚀  Server ready at ${PORT}`)
