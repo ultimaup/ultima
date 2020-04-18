8832c544691ad0f6802f66a322488ef7cdeca926
Josh Balfour <josh@joshbalfour.co.uk>
2020-04-18 16:58:23 +0100

    derp

--- mgmt/route.js
+++ mgmt/route.js
@@ -3,9 +3,9 @@ const {
     ROUTER_MGMT_ENDPOINT,
 } = process.env
 
-const set = ({ source, subdomain, destination }) => (
+const set = ({ source, subdomain, destination, extensions }) => (
     got.post(`${ROUTER_MGMT_ENDPOINT}/route`, {
-		json: { source, subdomain, destination },
+		json: { source, subdomain, destination, extensions },
 	}).json()
 )
 
