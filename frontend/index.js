6e8685fae81b62357b66c98c7a1794c7993b6d5f
Josh Balfour <josh@joshbalfour.co.uk>
2020-04-20 22:31:46 +0100

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const {
     PORT,
 } = process.env
@@ -7,7 +8,9 @@ const app = express()
 
 app.use(express.static('build'))
 app.use('/assets',express.static('build'))
-
+app.use((req, res, next) => {
+    return res.sendFile(path.resolve('./build/index.html'))
+});
 app.listen({ port: PORT }, () => {
     console.log(`🚀  Server ready at ${PORT}`)
 })
