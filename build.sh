330349271542a2d657f768629ceaaf0ef95549be
Sanguinaris <wieichdashasse@gmail.com>
2020-04-21 19:31:44 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ cd ..
 
 echo "Creating tarball"
 # Create backup
-tar -cpJf ../build-${DRONE_COMMIT_SHA}.tar.xz --exclude .git .
+tar -cpzf ../build-${DRONE_COMMIT_SHA}.tar.gz --exclude .git .
 
 echo "Uploading to S3"
 # upload backup to somewhere
@@ -60,13 +60,13 @@ host=drone.ultima.re
 s3_key="${S3_ACCESS_KEY}"
 s3_secret="${S3_ACCESS_SECRET}"
 
-resource="/build-artifacts/build-${DRONE_COMMIT_SHA}.tar.xz"
+resource="/build-artifacts/build-${DRONE_COMMIT_SHA}.tar.gz"
 content_type="application/octet-stream"
 date=`date -R`
 _signature="PUT\n\n${content_type}\n${date}\n${resource}"
 signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
 
-curl -4 -v -X PUT -T "../build-${DRONE_COMMIT_SHA}.tar.xz" \
+curl -4 -v -X PUT -T "../build-${DRONE_COMMIT_SHA}.tar.gz" \
           -H "Host: $host" \
           -H "Date: ${date}" \
           -H "Content-Type: ${content_type}" \
