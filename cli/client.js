24c7313154e454cbf2da46c377d3c44fc5fdeb70
Josh Balfour <josh@joshbalfour.co.uk>
2020-04-23 15:03:32 +0100

    wait for the LE cert to be allocated before trying to connect

--- cli/client.js
+++ cli/client.js
@@ -1,16 +1,38 @@
 const { context } = require('fetch-h2')
 
 const { fetch, disconnectAll } = context()
-
 const clientFetch = (endpoint, ...args) => fetch(`${rootEndpoint}${endpoint}`, ...args)
+
+let rootEndpoint
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const initSession = async (config) => {
     rootEndpoint = config.rootEndpoint
-    const res = await clientFetch(`/new-session`, {
-        method: 'post',
-    })
+    let data
+    let ctr = 0
 
-    return await res.json()
+    while (!data && ctr < 20) {
+        ctr++
+        try {
+            console.log('trying', rootEndpoint)
+            const res = await clientFetch(`/new-session`, {
+                method: 'post',
+            })
+            data = await res.json()
+        } catch (e) {
+            if (!e.message.includes('unable to verify the first certificate')) {
+                throw e
+            }
+        }
+        await wait(500)
+    }
+
+    if (!data) {
+        throw new Error('unable to connect to allocated development session')
+    }
+
+    return data
 }
 
 module.exports = {
