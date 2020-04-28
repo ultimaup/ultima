6f25c203bf4bd9db9a116a032db9d5565afc4510
Josh Balfour <josh@joshbalfour.co.uk>
2020-04-28 16:38:49 +0100

    separate out checkInUltimaFolder

---
+++ cli/commands/up/checkInUltimaFolder.js
@@ -0,0 +1,30 @@
+const git = require('simple-git/promise')()
+
+const getSSHHost = require('../login/getSSHHost')
+
+const checkInUltimaFolder = async ({ token }) => {
+    const isRepo = await git.checkIsRepo()
+
+    if (!isRepo) {
+        cli.log(`This doesn't look like a repo, are you in the right directory?`)
+        return false
+    }
+
+    const remotes = await git.getRemotes(true)
+
+    const sshHost = await getSSHHost({ token })
+
+    const ultimaRemote = remotes.find(remote => {
+        const url = remote.refs.push
+        return url.includes(sshHost.host)
+    })
+
+    if (!ultimaRemote) {
+        cli.log(`This doesn't look like an ultima project, are you in the right directory?`)
+        return false
+    }
+
+    return true
+}
+
+export default checkInUltimaFolder
