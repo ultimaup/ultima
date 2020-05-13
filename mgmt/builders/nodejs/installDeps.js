126edf8a53350aa9dad1066197fda49b7f629cf7
Josh Balfour <josh@joshbalfour.co.uk>
2020-05-13 18:57:48 +0100

    sub in our registry

--- mgmt/builders/nodejs/installDeps.js
+++ mgmt/builders/nodejs/installDeps.js
@@ -2,16 +2,21 @@ const fse = require('fs-extra')
 const path = require('path')
 const spawn = require('@expo/spawn-async')
 
+const {
+	npm_config_registry,
+	yarn_config_registry,
+} = process.env
+
 const getFileHash = async absPath => {
 	const { stdout } = await spawn(`sha1sum`, [absPath])
 	return stdout.split('/n')[0].split(' ')[0]
 }
 
-const restoreCache = async (cacheType, hash) => {
-	console.log(`restoring ${cacheType} cache for hash ${hash}`)
+// const restoreCache = async (cacheType, hash) => {
+// 	console.log(`restoring ${cacheType} cache for hash ${hash}`)
 
-	// TODO: actually do this
-}
+// 	// TODO: actually do this
+// }
 
 const installDeps = async (wkdir) => {
 	console.log('installing dependencies')
@@ -20,8 +25,10 @@ const installDeps = async (wkdir) => {
 
 	if (useYarn) {
 		console.log('using yarn')
-		const lockfileHash = await getFileHash(path.resolve(wkdir, 'yarn.lock'))
-		await restoreCache('yarn', lockfileHash)
+		// const lockfileHash = await getFileHash(path.resolve(wkdir, 'yarn.lock'))
+		// await restoreCache('yarn', lockfileHash)
+		await spawn('sed', [`s/=https://registry.yarnpkg.com/=${yarn_config_registry}/=g`, path.resolve(wkdir, 'yarn.lock')])
+		await spawn('sed', [`s/=https://registry.npmjs.org/=${npm_config_registry}/=g`, path.resolve(wkdir, 'yarn.lock')])
 		await spawn('yarn', ['install', '--frozen-lockfile' ,'--non-interactive'], { cwd: wkdir, stdio: 'inherit' })
 
 		return
@@ -29,8 +36,9 @@ const installDeps = async (wkdir) => {
 		const lockfileLocation = path.resolve(wkdir, 'package-lock.json')
 		if (await fse.pathExists(lockfileLocation)) {
 			console.log('using npm ci')
-			const lockfileHash = await getFileHash(lockfileLocation)
-			await restoreCache('npm', lockfileHash)
+			await spawn('sed', [`s/=https://registry.npmjs.org/=${npm_config_registry}/=g`, path.resolve(wkdir, 'package-lock.lock')])
+			// const lockfileHash = await getFileHash(lockfileLocation)
+			// await restoreCache('npm', lockfileHash)
 			await spawn('npm',['ci'], { cwd: wkdir, stdio: 'inherit' })
 
 			return
--- mgmt/development/nodejs/installDeps.js
+++ mgmt/development/nodejs/installDeps.js
@@ -2,6 +2,11 @@ const fse = require('fs-extra')
 const path = require('path')
 const spawn = require('@expo/spawn-async')
 
+const {
+	yarn_config_registry,
+	npm_config_registry,
+} = process.env
+
 const getFileHash = async absPath => {
 	const { stdout } = await spawn(`sha1sum`, [absPath])
 	return stdout.split('/n')[0].split(' ')[0]
@@ -29,6 +34,9 @@ const installDeps = async (wkdir, force, msgCb) => {
 		const lockfileHash = await getFileHash(path.resolve(wkdir, 'yarn.lock'))
 		await restoreCache('yarn', lockfileHash)
 
+		await spawn('sed', [`s/=https://registry.yarnpkg.com/=${yarn_config_registry}/=g`, path.resolve(wkdir, 'yarn.lock')])
+		await spawn('sed', [`s/=https://registry.npmjs.org/=${npm_config_registry}/=g`, path.resolve(wkdir, 'yarn.lock')])
+
 		const p = spawn('yarn', ['install', '--frozen-lockfile' ,'--non-interactive'], { cwd: wkdir, ignoreStdio: true })
 
 		p.child.stdout.on('data', msgCb)
@@ -41,6 +49,7 @@ const installDeps = async (wkdir, force, msgCb) => {
 		const lockfileLocation = path.resolve(wkdir, 'package-lock.json')
 		if (await fse.pathExists(lockfileLocation)) {
 			console.log('found package-lock.json so using npm ci')
+			await spawn('sed', [`s/=https://registry.npmjs.org/=${npm_config_registry}/=g`, path.resolve(wkdir, 'package-lock.lock')])
 			const lockfileHash = await getFileHash(lockfileLocation)
 			await restoreCache('npm', lockfileHash)
 			const p = spawn('npm',['ci'], { cwd: wkdir, ignoreStdio: true })
