c6157a1830e78c674d6b5cb3623fa1b81e1eda11
Josh Balfour <josh@joshbalfour.co.uk>
2020-05-25 19:45:18 +0100

    Revert "Update containers.js"
    This reverts commit 10f4c0e6ee38d42546aee82e6dda7661f7827536.

--- endpoints/containers.js
+++ endpoints/containers.js
@@ -92,7 +92,7 @@ const startContainerAndHealthcheck = async ({ requestId }, containerId) => {
 		const timeout = setTimeout(() => {
 			clearInterval(int)
 			reject(new Error('timeout'))
-		}, 10 * 1000) // timeout after 10 secs
+		}, 5 * 1000) // timeout after 5 secs
 
 		const int = setInterval(async () => {
 			const passed = await doHealthcheck(healthcheckUrl)
@@ -102,7 +102,7 @@ const startContainerAndHealthcheck = async ({ requestId }, containerId) => {
 				clearTimeout(timeout)
 				resolve(true)
 			}
-		}, 100)
+		}, 50)
 	})
 }
 
