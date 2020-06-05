c75487bda281240f75d52a135f65b0b6c3275d9a
Josh Balfour <josh@joshbalfour.co.uk>
2020-06-05 08:49:25 +0100

    always use certs

--- endpoints/containers/docker.js
+++ endpoints/containers/docker.js
@@ -1,10 +1,6 @@
 const Docker = require('dockerode')
 
-const {
-	IN_PROD = false,
-} = process.env
-
-const opts = !IN_PROD ? require('./keys') : {}
+const opts = require('./keys')
 
 const docker = new Docker(opts)
 
