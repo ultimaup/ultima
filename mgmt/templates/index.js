453f763ae266c39b96ee693a994027dffbb43d06
Josh Balfour <josh@joshbalfour.co.uk>
2020-07-14 14:40:03 +0100

    cache .ultima.yml templates in memory

--- mgmt/templates/index.js
+++ mgmt/templates/index.js
@@ -2,10 +2,16 @@ const path = require('path')
 const fse = require('fs-extra')
 const YAML = require('yaml')
 
+let cache
+
 const list = async () => {
+    if (cache) {
+        return cache
+    }
+
     const dir = await fse.readdir(path.resolve('templates'))
 
-    return await Promise.all(dir.filter(name => name !== 'index.js').map(async fname => {
+    cache = await Promise.all(dir.filter(name => name !== 'index.js').map(async fname => {
         const name = fname.split('.ultima.yml')[0]
         const template = await fse.readFile(path.resolve('templates', fname), 'utf-8')
         const t = YAML.parse(template)
@@ -16,6 +22,8 @@ const list = async () => {
             template: t[Object.keys(t)[0]]
         }
     }))
+
+    return cache
 }
 
 module.exports = {
